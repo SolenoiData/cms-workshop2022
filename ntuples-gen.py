5a30055bf1fc3784ca03b00df30eb4fd7466de64
Xavier Tintin <79267255+alextintin007@users.noreply.github.com>
2022-08-02 09:07:57 -0500

    Update ntuples-gen.py

--- ntuples-gen.py
+++ ntuples-gen.py
@@ -25,17 +25,10 @@ data['single_atop_t_chan'] = {}
 data['single_top_tW'] = {}
 data['wjets'] =  {}
 
-#def append(name1, name2, name3, data):
- #   data[str(name1)][str(name2)][str(name3)]=[]
-  #  data[str(name1)][str(name2)][str(name3)].append({
-   #     'recid': int(data[0]),
-    #    'nevents':int(data[1])
-    #})
-
 #24119
 data['data']['single muon'] = {}
-data['data']['single muon']['file']=[]
-data['data']['single muon']['file'].append({
+data['data']['single muon']['files']=[]
+data['data']['single muon']['files'].append({
     'path': findRecid("24119")[2],
     'nevents': findRecid("24119")[1]
 })
@@ -43,8 +36,8 @@ data['data']['single muon']['nevents_total']=findRecid("24119")[1]
 
 #24120
 data['data']['single electron'] = {}
-data['data']['single electron']['file']=[]
-data['data']['single electron']['file'].append({
+data['data']['single electron']['files']=[]
+data['data']['single electron']['files'].append({
     'path': findRecid("24120")[2],
     'nevents': findRecid("24120")[1]
 })
@@ -52,8 +45,8 @@ data['data']['single electron']['nevents_total']=findRecid("24120")[1]
 
 #19980
 data['ttbar']['nominal']={}
-data['ttbar']['nominal']['file']=[]
-data['ttbar']['nominal']['file'].append({
+data['ttbar']['nominal']['files']=[]
+data['ttbar']['nominal']['files'].append({
     'path': findRecid("19980")[2],
     'nevents': findRecid("19980")[1]
 })
@@ -61,8 +54,8 @@ data['ttbar']['nominal']['nevents_total']=findRecid("19980")[1]
 
 #19983
 data['ttbar']['scaledown']={}
-data['ttbar']['scaledown']['file']=[]
-data['ttbar']['scaledown']['file'].append({
+data['ttbar']['scaledown']['files']=[]
+data['ttbar']['scaledown']['files'].append({
     'path': findRecid("19983")[2],
     'nevents': findRecid("19983")[1]
 })
@@ -70,8 +63,8 @@ data['ttbar']['scaledown']['nevents_total']=findRecid("19983")[1]
 
 #19985
 data['ttbar']['scaleup']={}
-data['ttbar']['scaleup']['file']=[]
-data['ttbar']['scaleup']['file'].append({
+data['ttbar']['scaleup']['files']=[]
+data['ttbar']['scaleup']['files'].append({
     'path': findRecid("19985")[2],
     'nevents': findRecid("19985")[1]
 })
@@ -79,8 +72,8 @@ data['ttbar']['scaleup']['nevents_total']=findRecid("19985")[1]
 
 #19949
 data['ttbar']['ME_var']={}
-data['ttbar']['ME_var']['file']=[]
-data['ttbar']['ME_var']['file'].append({
+data['ttbar']['ME_var']['files']=[]
+data['ttbar']['ME_var']['files'].append({
     'path': findRecid("19949")[2],
     'nevents': findRecid("19949")[1]
 })
@@ -88,8 +81,8 @@ data['ttbar']['ME_var']['nevents_total']=findRecid("19949")[1]
 
 #19999
 data['ttbar']['PS_var']={}
-data['ttbar']['PS_var']['file']=[]
-data['ttbar']['PS_var']['file'].append({
+data['ttbar']['PS_var']['files']=[]
+data['ttbar']['PS_var']['files'].append({
     'path': findRecid("19999")[2],
     'nevents': findRecid("19999")[1]
 })
@@ -97,8 +90,8 @@ data['ttbar']['PS_var']['nevents_total']=findRecid("19999")[1]
 
 #19397
 data['single_top_t_chan']['nominal'] = {}
-data['single_top_t_chan']['nominal']['file']=[]
-data['single_top_t_chan']['nominal']['file'].append({
+data['single_top_t_chan']['nominal']['files']=[]
+data['single_top_t_chan']['nominal']['files'].append({
     'path': findRecid("19397")[2],
     'nevents': findRecid("19397")[1]
 })
@@ -106,8 +99,8 @@ data['single_top_t_chan']['nominal']['nevents_total']=findRecid("19397")[1]
 
 #19407
 data['single_atop_t_chan']['nominal'] = {}
-data['single_atop_t_chan']['nominal']['file']=[]
-data['single_atop_t_chan']['nominal']['file'].append({
+data['single_atop_t_chan']['nominal']['files']=[]
+data['single_atop_t_chan']['nominal']['files'].append({
     'path': findRecid("19407")[2],
     'nevents': findRecid("19407")[1]
 })
@@ -115,14 +108,14 @@ data['single_atop_t_chan']['nominal']['nevents_total']=findRecid("19407")[1]
 
 #19419
 data['single_top_tW']['nominal'] = {}
-data['single_top_tW']['nominal']['file']=[]
-data['single_top_tW']['nominal']['file'].append({
+data['single_top_tW']['nominal']['files']=[]
+data['single_top_tW']['nominal']['files'].append({
     'path': findRecid("19419")[2],
     'nevents': findRecid("19419")[1]
 })
 
 #19412
-data['single_top_tW']['nominal']['file'].append({
+data['single_top_tW']['nominal']['files'].append({
     'path': findRecid("19412")[2],
     'nevents': findRecid("19412")[1]
 })
@@ -130,12 +123,12 @@ data['single_top_tW']['nominal']['nevents_total']= findRecid("19419")[1]+findRec
 
 #20548
 data['wjets']['nominal'] = {}
-data['wjets']['nominal']['file']=[]
-data['wjets']['nominal']['file'].append({
+data['wjets']['nominal']['files']=[]
+data['wjets']['nominal']['files'].append({
     'path': findRecid("20548")[2],
     'nevents': findRecid("20548")[1]
 })
 data['wjets']['nominal']['nevents_total']=findRecid("20548")[1]
 
-with open('ntuples.json', 'w') as file:
-    json.dump(data, file)
+with open('ntuples.json', 'w') as files:
+    json.dump(data, files)
